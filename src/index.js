fa9fc1d3e0703c9f3a3eaa41cec4a5899002acab
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2023-06-21 15:35:50 -0300

    chore: add zod validation for .env vars

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-//Voce deve rodar os testes usando:  npm test
-//Para testar a aplicação, rode: npm run dev
-
-//mais infos
-//https://github.com/ZijianHe/koa-router
-
-// todas as configuraçoes devem ser passadas via environment variables
-const PORT = process.env.PORT || 3000;
-
 import Koa from 'koa'
-import Router from 'koa-router'
 import userRoutes from './http/controllers/users/routes.js'
+import { env } from './env/index.js';
 
 const koa = new Koa();
-let router = new Router();
 
 koa
   .use(userRoutes.routes())
-  .use(router.routes())
-  .use(router.allowedMethods());
 
-koa.listen(PORT);
+koa.listen(env.PORT);
